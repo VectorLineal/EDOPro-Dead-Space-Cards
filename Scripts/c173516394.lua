b2572e9d0b1c0b794f909ad904a0116d4290e760
VectorLineal <jnobzaa@unal.edu.co>
2024-04-17 12:29:52 -0500

    Added Necromorph Divider Components

---
+++ Scripts/c173516394.lua
@@ -0,0 +1,63 @@
+--Necromorph Divider Components
+local s,id=GetID()
+function s.initial_effect(c)
+	--Equip when Tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetCost(s.eqcost)
+	e1:SetTarget(s.eqtg)
+	e1:SetOperation(s.eqop)
+	c:RegisterEffect(e1)
+end
+function s.disfilter(c,tp)
+	return c:IsRace(RACE_ZOMBIE)
+end
+function s.eqcost(e,tp,eg,ep,ev,re,r,rp,chk)
+  local c=e:GetHandler()
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.disfilter,2,false,nil,c) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.disfilter,2,2,false,nil,c)
+	Duel.Release(g,REASON_COST)
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and aux.CheckStealEquip(chkc,e,tp) end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,aux.CheckStealEquip,tp,0,LOCATION_MZONE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.eqlimit(e,c)
+	return e:GetOwner()==c
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and not c:IsStatus(STATUS_BATTLE_DESTROYED) and aux.CheckStealEquip(tc,e,tp) and c:IsRelateToEffect(e) and Duel.Equip(tp,c,tc) then
+		--Add Equip limit
+		local e1=Effect.CreateEffect(tc)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(s.eqlimit)
+		c:RegisterEffect(e1)
+    --Take control
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_EQUIP)
+		e2:SetCode(EFFECT_SET_CONTROL)
+		e2:SetValue(tp)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+		c:RegisterEffect(e2)
+    --Change it to Zombie monster
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_CHANGE_RACE)
+    e3:SetValue(RACE_ZOMBIE)
+    e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+    tc:RegisterEffect(e3)
+	end
+end
