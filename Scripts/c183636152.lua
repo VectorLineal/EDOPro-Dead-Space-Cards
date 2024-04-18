02583992d0f1eb45138401b21241dc26ef9db151
VectorLineal <jnobzaa@unal.edu.co>
2024-04-17 19:30:09 -0500

    added Necromorph Hunter and Necromorph Nest

---
+++ Scripts/c183636152.lua
@@ -0,0 +1,82 @@
+--Necromorph Nest
+local s,id=GetID()
+function s.initial_effect(c)
+  --Enable Necro counters collection
+  c:EnableCounterPermit(0x1ff)
+  --synchro summon
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_ZOMBIE),1,1,Synchro.NonTunerEx(Card.IsRace,RACE_ZOMBIE),1,99)
+	c:EnableReviveLimit()
+  --special summon monster from any graveyard
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1, id)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCost(s.thcost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+  --add Necro counters on special summon
+  local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.addtg)
+	e2:SetOperation(s.addop)
+	c:RegisterEffect(e2)
+  --Destruction replacement
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.desreptg)
+	c:RegisterEffect(e3)
+end
+--Return 1 card to the hand
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsCanRemoveCounter(tp,1,0,0x1ff,1,REASON_COST) end
+	Duel.RemoveCounter(tp,1,0,0x1ff,2,REASON_COST)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+--add list to count Necro Counters
+s.counter_place_list={0x1ff}
+--specify to add Necro counters
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,2,0,0x1ff)
+end
+--add the 'label' amount of Necro counters to this card
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1ff,2)
+	end
+end
+--Avoid destruction by removing 1 Necro counter
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+	return not c:IsReason(REASON_REPLACE)
+		and c:IsCanRemoveCounter(tp,0x1ff,1,REASON_COST) end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		c:RemoveCounter(tp,0x1ff,1,REASON_COST)
+		return true
+	else return false end
+end
---
+++ Scripts/c183645272.lua
@@ -0,0 +1,74 @@
+--Necromorph Hunter
+local s,id=GetID()
+function s.initial_effect(c)
+  --Enable Necro counters collection
+  c:EnableCounterPermit(0x1ff)
+  --synchro summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+  --Can only control 1
+  c:SetUniqueOnField(1,0,id)
+  --special summon each Standby fase
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+  --add Necro counters on special summon
+  local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.addtg)
+	e2:SetOperation(s.addop)
+	c:RegisterEffect(e2)
+  --Destruction replacement
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.desreptg)
+	c:RegisterEffect(e3)
+end
+--Special summon from any GY on Standby phase
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+--add list to count Necro Counters
+s.counter_place_list={0x1ff}
+--specify to add Necro counters
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x1ff)
+end
+--add the 'label' amount of Necro counters to this card
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1ff,1)
+	end
+end
+--Avoid destruction by removing 1 Necro counter
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+	return not c:IsReason(REASON_REPLACE)
+		and c:IsCanRemoveCounter(tp,0x1ff,1,REASON_COST) end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		c:RemoveCounter(tp,0x1ff,1,REASON_COST)
+		return true
+	else return false end
+end
