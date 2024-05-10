99e52f3590821ab4f2e23139050a959d25114607
VectorLineal <jnobzaa@unal.edu.co>
2024-05-10 09:53:13 -0500

    added Necromorph Twitcher and Necromorph Pregnant

---
+++ Scripts/c190375782.lua
@@ -0,0 +1,70 @@
+--Necromorph Twitcher
+local s,id=GetID()
+function s.initial_effect(c)
+  --Enable Necro counters collection
+  c:EnableCounterPermit(0x1ff)
+  --synchro summon
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_ZOMBIE),2,2)
+	c:EnableReviveLimit()
+  --multi attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
+  --add Necro counters on special summon
+  local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.addtg)
+	e2:SetOperation(s.addop)
+	c:RegisterEffect(e2)
+  --Destruction replacement
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.desreptg)
+	c:RegisterEffect(e3)
+end
+--Multi attack value
+function s.val(e,c)
+	local necroAmount = Duel.GetCounter(e:GetHandlerPlayer(),1,0,0x1ff)
+  if necroAmount > 3 then
+    necroAmount = 3
+  end
+  necroAmount = necroAmount - 1
+  if necroAmount < 0 then
+    necroAmount = 0
+  end
+	return necroAmount
+end
+--add list to count Necro Counters
+s.counter_place_list={0x1ff}
+--specify to add Necro counters
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x1ff)
+end
+--add the 'label' amount of Necro counters to this card
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1ff,1)
+	end
+end
+--Avoid destruction by removing 1 Necro counter
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+	return not c:IsReason(REASON_REPLACE)
+		and c:IsCanRemoveCounter(tp,0x1ff,1,REASON_COST) end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		c:RemoveCounter(tp,0x1ff,1,REASON_COST)
+		return true
+	else return false end
+end
---
+++ Scripts/c34729393.lua
@@ -0,0 +1,100 @@
+--Necromorph Pregnant
+local s,id=GetID()
+function s.initial_effect(c)
+  --Enable Necro counters collection
+  c:EnableCounterPermit(0x1ff)
+  --synchro summon
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_ZOMBIE),2,2)
+	c:EnableReviveLimit()
+  --special summon Swarmer on send to GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,{id,1})
+  e1:SetCondition(s.tdcondition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+  --add Necro counters on special summon
+  local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.addtg)
+	e2:SetOperation(s.addop)
+	c:RegisterEffect(e2)
+  --Destruction replacement
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.desreptg)
+	c:RegisterEffect(e3)
+end
+s.listed_names={id,2309538}
+--Special summon divider components from hand, deck or GY
+function s.tdcondition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_LINK) and rp==1-tp
+end
+function s.filter(c,e,tp)
+	return c:IsCode(2309538) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:GetCounter(0x1ff) > 0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	if ft>3 then ft=3 end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,ft,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+    --Lock Into Zombies
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+    e1:SetTargetRange(1,0)
+    e1:SetTarget(s.splimit)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+	end
+end
+--Filter The Summon Limitation
+function s.splimit(e,c)
+	return not c:IsRace(RACE_ZOMBIE)
+end
+--add list to count Necro Counters
+s.counter_place_list={0x1ff}
+--specify to add Necro counters
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x1ff)
+end
+--add the 'label' amount of Necro counters to this card
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0x1ff,1)
+	end
+end
+--Avoid destruction by removing 1 Necro counter
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+	return not c:IsReason(REASON_REPLACE)
+		and c:IsCanRemoveCounter(tp,0x1ff,1,REASON_COST) end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		c:RemoveCounter(tp,0x1ff,1,REASON_COST)
+		return true
+	else return false end
+end
